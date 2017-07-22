051164e4a1fdf335ea1f801050fb60e89c249db7
beleychev.ilya <beleychev.ilya@otr.ru>
2017-07-22 09:43:36 +0300

    Try to add RPC // 403 exception problem

--- src/main/java/ru/beleychev/notes/client/NotesGwtApp.java
+++ src/main/java/ru/beleychev/notes/client/NotesGwtApp.java
@@ -1,9 +1,13 @@
 package ru.beleychev.notes.client;
 
+import com.google.gwt.cell.client.EditTextCell;
+import com.google.gwt.cell.client.TextCell;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.DataGrid;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -12,7 +16,11 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.view.client.ListDataProvider;
 import ru.beleychev.notes.client.dto.UserDTO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Main gwt class with UI building
@@ -47,9 +55,36 @@ public class NotesGwtApp implements EntryPoint {
 	private DataGrid<UserDTO> notesList= new DataGrid<>();
 	private ScrollPanel listPanel = new ScrollPanel();
 
+	private NotesGwtServiceAsync notesGwtServiceAsync = GWT.create(NotesGwtService.class);
 	@Override
 	public void onModuleLoad() {
-		//notesList.addColumn(new Column<UserDTO, String>(), "First Name");
-		RootPanel.get();
+		Column<UserDTO, String> firstNameColumn = new Column<UserDTO, String>(new TextCell()) {
+			@Override
+			public String getValue(UserDTO object) {
+				return object.getFirstName();
+			}
+		};
+		firstNameColumn.setSortable(true);
+		notesList.setWidth("100%");
+		notesList.addColumn(firstNameColumn, "First name");
+		if (notesGwtServiceAsync == null) {
+			notesGwtServiceAsync = GWT.create(NotesGwtService.class);
+		}
+		AsyncCallback<List<UserDTO>> callback = new AsyncCallback<List<UserDTO>>() {
+			@Override
+			public void onFailure(Throwable caught) {
+				//todo something
+			}
+
+			@Override
+			public void onSuccess(List<UserDTO> result) {
+				notesList.setRowData(0, result);
+			}
+		};
+		notesGwtServiceAsync.listUsers(callback);
+
+		mainPanel.setWidth("100%");
+		mainPanel.add(notesList);
+		RootPanel.get().add(mainPanel);
 	}
 }
