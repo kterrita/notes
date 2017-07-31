554563a54d153c4e1f78781603b949dae6c234f0
beleychev.ilya <beleychev.ilya@otr.ru>
2017-07-31 14:15:21 +0300

    Мутим CSS, пока с проблемами)

--- src/main/java/ru/beleychev/notes/server/domain/Note.java
+++ src/main/java/ru/beleychev/notes/server/domain/Note.java
@@ -1,11 +1,27 @@
 package ru.beleychev.notes.server.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * Note entity
  * @author beleychev.ilya 21.07.2017   15:21
  */
 
-public class Note implements Serializable{
+@Entity
+@Table(name = "notes")
+public class Note implements Serializable {
+
+	private Long id;
+	private UUID uuid;
+	private boolean favorite;
+	private Date dateCreated;
+	private NoteType noteType;
+	private String content;
+	private User user;
+	private NoteState noteState;
+	private String title;
 }
---
+++ src/main/java/ru/beleychev/notes/server/domain/NoteType.java
@@ -0,0 +1,8 @@
+package ru.beleychev.notes.server.domain;
+
+/**
+ * TODO: comment
+ * @author beleychev.ilya 31.07.2017   13:42
+ */
+public class NoteType {
+}
